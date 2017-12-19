438d7085cda4376ef5867f3a86eb29b63bb4f367
dslyecix <31083701+dslyecix@users.noreply.github.com>
2017-12-19 13:38:18 -0500

    Got it working!
    Eliminated the memory leak.
    
    Custom rooms are generating perfectly.

---
+++ Assets/Scripts/GeneratorDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratorDoor : MonoBehaviour {
+    public bool open = true;
+    public GameObject voxelOwner;
+    public GeneratorDoor sharedDoor;
+
+    public Door door;
+}
+
