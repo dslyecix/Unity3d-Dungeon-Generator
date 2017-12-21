56467955782e261150143085e608027106227dfa
dslyecix <31083701+dslyecix@users.noreply.github.com>
2017-12-21 15:45:03 -0500

    did stuff
    changed a few things,. walls and floor line up differently now.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+ 
+public class CameraController : MonoBehaviour {
+ 
+    public float movementSpeed;
+ 
+    void Update () {
+        movementSpeed = 1f;
+
+        if ( Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = 0.5f;
+        }
+
+        transform.position = transform.position + (transform.right * Input.GetAxisRaw("Horizontal") * movementSpeed) + (transform.forward * Input.GetAxisRaw("Vertical") * movementSpeed) + (transform.up * Input.GetAxisRaw("Depth") * movementSpeed / 3f);
+        transform.eulerAngles += new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0f);
+    }
+}
+ 
