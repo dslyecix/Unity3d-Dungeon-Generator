36fcbac9f7c0c375e52a40378ed492499019b131
dslyecix <31083701+dslyecix@users.noreply.github.com>
2018-02-03 21:31:03 -0500

    v0.0.2
    wowwwee!
    
    Upped the lighting, added rudimentary UI system, fixed up the animation state machine, began stat system

---
+++ Assets/Game Resources/Scripts/CharacterStats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    
+    public int currentHealth { get; private set; }
+    public int currentMana { get; private set; }
+    public Stat maxHealth;
+    public Stat healthRegen;
+    public Stat damage;
+    public Stat armour;
+    public Stat moveSpeed;
+    public Stat stamina;
+    public Stat staminaRegen;
+    public Stat maxMana;
+    public Stat manaRegen;
+
+    void Awake ()
+    {
+        currentHealth = maxHealth.GetValue();
+    }
+
+    void Update( )
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+            Debug.Log(currentHealth);
+        }
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damage -= armour.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes" + damage + " damage.");
+
+        if (currentHealth <=0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ Assets/Game Resources/Scripts/Stat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat {
+
+    [SerializeField]
+	private int baseValue;
+
+    private List<int> addModifiers = new List<int>();
+    private List<float> multiplyModifiers = new List<float>();
+
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+
+        addModifiers.ForEach(x => finalValue += x);
+        multiplyModifiers.ForEach(x => finalValue *= (int)x);
+        return finalValue;
+    }
+
+    public void AddMultiplyModifier(float modifier)
+    {
+        if (modifier != 0) multiplyModifiers.Add(modifier);
+    }
+
+    public void AddModifier(int modifier)
+    {
+         if (modifier != 0) addModifiers.Add(modifier);
+    }
+
+    public void RemoveModifier(int modifier)
+    {
+        if (modifier != 0) addModifiers.Remove(modifier);
+    }
+
+    public void RemoveMultiplyModifier(int modifier)
+    {
+        if (modifier != 0) multiplyModifiers.Remove(modifier);
+    }
+}
